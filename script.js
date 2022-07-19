bd46adfe631bad4553ef7a42f425c0a8f36b433b
Altynay-ayyao <77488933+Altynay-ayyao@users.noreply.github.com>
2022-07-19 16:16:05 +0300

    projects added

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/* document.getElementById('save').addEventListener('', function () {
-  document.querySelector('.bg-modal').style.display = 'flex';
-});
- */
-/* document.querySelector('#thanks_close').addEventListener('click', function () {
-  document.querySelector('.bg-modal').style.display = 'none';
-});
-
-document.querySelector('#close').addEventListener('click', function () {
-  document.querySelector('.body_wrapper').style.display = 'none';
-});
- */
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('.bg-modal').style.display = 'flex';
 });
